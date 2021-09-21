e44d63e94226521d552ba11dabce5c766061891d
santhosh-annam <santhosh.annam@devrabbit.com>
2021-09-21 18:47:42 +0530

    latest execution

---
+++ src/test/java/web/idlyexpress/idlyexpressweb/webpageclasses/MangoLassiSelection.java
@@ -0,0 +1,9 @@
+package web.idlyexpress.idlyexpressweb.webpageclasses;
+
+public class MangoLassiSelection { 
+
+	public String _mangoLassi = "//li[@id='product_item_1_2']/a/div/div[2]/div/h6[1]/span";
+	public String _iconiconplusc = "//div[@id='increase']/span";
+	public String _addtoCart = "//span[@id='add_cart_text']";
+
+ }
