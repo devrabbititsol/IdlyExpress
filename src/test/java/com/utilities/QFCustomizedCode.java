d0a70e0264ff015ae67e99eb6dc445c4bb21e6b9
santhosh-annam <santhosh.annam@devrabbit.com>
2021-09-21 15:04:17 +0530

    first commit

---
+++ src/test/java/com/utilities/QFCustomizedCode.java
@@ -0,0 +1,12 @@
+package com.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class QFCustomizedCode {
+	
+	public static String customCode(WebDriver driver, String xpath, String fieldName, int i) {
+		
+		return "";
+	}
+
+}
