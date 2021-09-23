b6bd25bb4b52ada9088ad0b129357764a49d3846
santhosh-annam <santhosh.annam@devrabbit.com>
2021-09-23 22:04:38 +0530

    latest execution

---
+++ src/test/java/web/idlyexpress/web/webpageclasses/IdlySelection.java
@@ -0,0 +1,10 @@
+package web.idlyexpress.web.webpageclasses;
+
+public class IdlySelection { 
+
+	public String _idlyV = "//li[@id='product_item_0_0']/a/div/div[2]/div/h6[1]/span";
+	public String _iconiconplusc = "//div[@id='increase']/span";
+	public String _iconiconminusc = "//div[@id='decrease']/span";
+	public String _addtoCart = "//span[@id='add_cart_text']";
+
+ }
